7e3aeeb639fdaf8a84bef51350d59dd49c3b84f6
Keith Allan <keithallan01@gmail.com>
2019-02-15 23:34:44 +0000

    person class added and tested

--- src/main/java/Venue.java
+++ src/main/java/Venue.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
+
 public class Venue {
     private String name;
+    private int capacity;
+    private ArrayList<Person> audience;
 
-    public Venue(String name){
+    public Venue(String name, int capacity){
         this.name = name;
+        this.capacity = capacity;
+        this.audience = new ArrayList<>();
     }
 
     public String getName(){
         return this.name;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int audienceCount(){
+        return this.audience.size();
     }
 }
--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.lang.reflect.Member;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class VenueTest {
     Venue venue;
@@ -15,7 +16,7 @@ public class VenueTest {
 
     @Before
     public void before(){
-        venue = new Venue("Sneaky Pete's");
+        venue = new Venue("Sneaky Pete's", 50);
         band = new Band("Jimmy Shand Band", "folk");
         musician1 = new Musician("Kris Drever", InstrumentType.GUITAR);
         musician2 = new Musician("John McCusker", InstrumentType.VIOLIN);
@@ -27,4 +28,15 @@ public class VenueTest {
     public void venueHasName(){
         assertEquals("Sneaky Pete's", venue.getName());
     }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(50, venue.getCapacity());
+    }
+    @Test
+    public void venueStartsOffEmpty(){
+        assertEquals(0, venue.audienceCount());
+    }
+
+
 }
